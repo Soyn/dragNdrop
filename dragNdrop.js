6ba845f743df9d2ae0ee17fe743f1c30748924a6
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2016-12-13 12:47:25 +0100

    Update 2016-12-13T11:47:24.285Z

--- dragNdrop.js
+++ dragNdrop.js
@@ -456,5 +456,41 @@ function dragNdrop(options) {
       return false;
     };
   }
+  // Array.prototype.every
+  // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every#Polyfill
+  if (!Array.prototype.every) {
+    console.log('WARN: dragNdrop: polyfill for Array.prototype.every used');
+    Array.prototype.every = function(callbackfn, thisArg) {
+      'use strict';
+      var T, k;
+
+      if (this == null) {
+        throw new TypeError('this is null or not defined');
+      }
+
+      var O = Object(this);
+      var len = O.length >>> 0;
+      if (typeof callbackfn !== 'function') {
+        throw new TypeError();
+      }
+      if (arguments.length > 1) {
+        T = thisArg;
+      }
+      k = 0;
+      while (k < len) {
+
+        var kValue;
+        if (k in O) {
+          kValue = O[k];
+          var testResult = callbackfn.call(T, kValue, k, O);
+          if (!testResult) {
+            return false;
+          }
+        }
+        k++;
+      }
+      return true;
+    };
+  }
 
 }
